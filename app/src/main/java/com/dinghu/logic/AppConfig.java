fcc5440ef6efa2daa5a7c1e6ccda4818ddbce2ad
jakechen <903475400@qq.com>
2015-10-07 19:20:16 +0800

    增加测试数据管理

--- app/src/main/java/com/dinghu/logic/AppConfig.java
+++ app/src/main/java/com/dinghu/logic/AppConfig.java
@@ -8,7 +8,10 @@ package com.dinghu.logic;
  * @since 2015/9/19 16:48
  */
 public class AppConfig {
-    public static final boolean IS_DEBUG = false;
+    public static final boolean IS_DEBUG = true;
 
-    public static final int PUSH_MESSAGE_TIME_SPIT = 10*60 * 1000;
+
+    public static final int PUSH_MESSAGE_TIME_SPIT_TEST = 10 * 1000;
+    public static final int PUSH_MESSAGE_TIME_SPIT_RELEASE = 3 * 60 * 1000;
+    public static final int PUSH_MESSAGE_TIME_SPIT = IS_DEBUG ? PUSH_MESSAGE_TIME_SPIT_TEST : PUSH_MESSAGE_TIME_SPIT_RELEASE;
 }
